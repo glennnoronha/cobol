b051142f2ad1f2a754580d78813b407bdc919a8b
glennnoronha <glenn.noronha@yahoo.com>
2025-01-29 12:59:19 -0600

    first program

---
+++ prog01.cob
@@ -0,0 +1,15 @@
+      * this is a comment 
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. prog01.
+       AUTHOR. Glenn Noronha.
+       INSTALLATION. WTAMU CS 3322.
+       DATE-WRITTEN. 01/27/2025.
+       DATE-COMPILED. 01/27/2025.
+       SECURITY. Non-confidential.
+      * another comment, can be placed in any row
+
+       PROCEDURE DIVISION.
+      * can put a statement in here (goes in area b)
+           DISPLAY "Hello COBOL".
+           STOP RUN.
+           
