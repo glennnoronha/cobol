2e92c14a3562bc2c63bb97dfd05efaf32d6a2a90
Glenn Noronha <96216394+glennnoronha@users.noreply.github.com>
2025-03-26 14:17:01 -0500

    Update perfvar.cob

--- loops/perfvar.cob
+++ loops/perfvar.cob
@@ -4,11 +4,12 @@
        DATA DIVISION.
        WORKING-STORAGE SECTION.
        
-       01 WS-X1 PIC 9.
-       01 WS-X2 PIC 9.
-       
+       01 WS-X1 PIC 9 VALUE 1.
+       01 WS-X2 PIC 99.
+       01 WS-X3 PIC 9 VALUE 2.
+  
        PROCEDURE DIVISION.
-           PERFORM VARYING WS-X2 FROM 1 BY 1 UNTIL WS-X2 = 3
+           PERFORM VARYING WS-X2 FROM WS-X1 BY WS-X3 UNTIL WS-X2 > 9
               DISPLAY WS-X2
            END-PERFORM.
 
